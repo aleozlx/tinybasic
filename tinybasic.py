81208cc763d200c7fa4bb36166f7242a21a62f48
Ray Sohn <raymondsohn@gmail.com>
2012-12-06 16:34:14 -0500

    FUBAR!

--- tinybasic.py
+++ tinybasic.py
@@ -1,52 +1,65 @@
 from peglet import *
 from pprint import *
 import re
+import operator as op
 
 
 grammar = r"""
 lines       = _ line _ lines
             | _ line
 line        = number _ stmt                     hug
-            | simple_stmt                       hug
+            | stmt                              hug
 
-stmt        = print_stmt                        hug
-            | if_stmt                           hug
-            | goto_stmt                         hug
-            | input_stmt                        hug
-            | let_stmt                          hug
-            | gosub_stmt                        hug
-            | simple_stmt                       hug
+stmt        = STMT print_stmt                   hug
+            | STMT if_stmt                      hug
+            | STMT goto_stmt                    hug
+            | STMT input_stmt                   hug
+            | STMT let_stmt                     hug
+            | STMT gosub_stmt                   hug
+            | STMT simple_stmt                  hug
 
-print_stmt  = (PRINT) _ expr_list
-if_stmt     = (IF) _ relop_expr _ (THEN) _ stmt
-goto_stmt   = (GOTO) _ expr
-input_stmt  = (INPUT) _ var_list
-let_stmt    = (LET) _ var_expr
-gosub_stmt  = (GOSUB) _ expr
-simple_stmt = (RETURN|CLEAR|LIST|RUN|END)
+print_stmt  = (PRINT\b) _ expr_list
+if_stmt     = (IF\b) _ relop_expr _ (THEN\b) _ stmt
+goto_stmt   = (GOTO\b) _ expr
+input_stmt  = (INPUT\b) _ var_list
+let_stmt    = (LET\b) _ var_expr
+gosub_stmt  = (GOSUB\b) _ expr
+simple_stmt = (RETURN\b|CLEAR\b|LIST\b|RUN\b|END\b)
 
-term        = var
-            | NUM unop                          hug
-            | NUM number                        hug
-
-eq          = EQOP (=)                          hug
-relop       = (<|>|=)
-relop_expr  = expr _ relop _ expr               hug
-
-
-binop       = BINOP (\+|\-|\*|\/)               hug
-unop        = (\+|\-) number                    join
+expr        = EXP term _ plus_minus _ expr      hug
+            | EXP term                          hug
 
 expr_list   = expr_or_str _ , _ expr_list
             | expr_or_str
-expr        = term _ binop _ expr               hug
-            | term
-expr_or_str = EXP expr                          hug
+expr_or_str = expr                              
             | STR string                        hug
+
+term        = factor _ mult_div _ term          hug
+            | factor
+
+factor      = var 
+            | NUM number                        hug
+            | l_paren _ expr _ r_paren          hug
+
+l_paren     = (?:\()
+r_paren     = (?:\)) 
+
+assop       = ASSOP (=)                         hug
+
+plus_minus  = plus | minus
+mult_div    = mult | div
+plus        = BINOP (\+)                        hug
+minus       = BINOP (\-)                        hug
+mult        = BINOP (\*)                        hug
+div         = BINOP (\/)                        hug
+
+relop       = (<|>|=)
+relop_expr  = expr _ relop _ expr
+
 
 var_list    = var _ , _ var_list
             | var
-var_expr    = var _ eq _ expr
+var_expr    = var _ assop _ expr
 var         = VAR ([A-Z])                       hug
 
 string      = " chars " _                       join
@@ -63,6 +76,7 @@ esc_char    = \\(['"/\\])
             | \\([bfnrt])                       escape
 
 number      = (\d+)
+            | (?:-) number
 
 _           = \s*
 """
@@ -81,56 +95,116 @@ class TinyBasic:
                              STR=Tag("STR"),
                              NUM=Tag("NUM"),
                              VAR=Tag("VAR"),
+                             STMT=Tag("STMT"),
                              BINOP=Tag("BINOP"),
-                             EQOP=Tag("EQOP"))
-        self.memory = {}
+                             ASSOP=Tag("ASSOP"))
+        self.operators = {"+": op.add,
+                          "-": op.sub
+                
+                }
         self.symbols = {}
+
     def parse(self, program):
         self.ast = self.parser(program)
+
     def eval(self):
-        for line in self.ast:
-            n, i = int(line[0]), line[1]
-            self.memory[n] = i
-        for n in sorted(self.memory):
-            i = self.memory[n]
-            stmt = i[0]
-            args = i[1:]
-            if stmt == "PRINT": self.print_stmt(args)
-            elif stmt == "LET": self.let_stmt(args)
-        #pprint(self.ast)
+        for line in self.ast: 
+            if len(line) == 1:
+                pass
+            else:
+                line_number = line[0]
+                stmt = line[1][1:]
+                self.eval_stmt(stmt)
 
-    def print_stmt(self, args):
-        result = []
-        for arg in args:
-            head, tail = arg[0], arg[1]
-            if head == "STR":
-                result.append(tail)
-            elif head == "EXP":
-                expr = []
-                if tail[0] == "VAR":
-                    expr.append(tail[1])
-                else:
-                    for t in tail:
-                        if t[0] == "NUM" or t[0] == "BINOP":
-                            expr.append(t[1])
-                        elif t[0] == "VAR":
-                            expr.append(self.symbols[t[1]])
+        print self.symbols
+        
+    def eval_stmt(self, stmt):
+        keyword = stmt[0]
+        body = stmt[1:]
+        if keyword == "PRINT":
+            self.eval_print_stmt(body)
+        elif keyword == "LET":
+            self.eval_let_stmt(body)
+        elif keyword == "INPUT":
+            self.eval_input_stmt(body)
+        elif keyword == "IF":
+            self.eval_if_stmt(body)
 
-                result.append(str(eval("".join(expr))))
-        print " ".join(result)
+    def eval_if_stmt(self, body):
 
-    def let_stmt(self, args):
-        k = args[0][1]
-        n = args[2][1]
-        self.symbols[k] = n
+        expr1    = body[0]
+        operator = body[1]
+        expr2    = body[2]
+        stmt     = body[4]
+        
+
+    def eval_input_stmt(self, var_list):
+        for var in var_list:
+            name = self.eval_var(var)
+            self.symbols[name] = raw_input("?")
+    
+    def eval_let_stmt(self, body):
+        self.eval_var_expr(body)
+        
+    def eval_print_stmt(self, expr_list):
+        output = ""
+        for expr in expr_list:
+            output += str(self.eval_expr_or_str(expr))
+        print output
+
+    def eval_expr_or_str(self, expr):
+        if expr[0] == "STR":
+            return expr[1]
+        else:
+            return self.eval_expr(expr)
+            
+    def eval_expr(self, expr):
+        expr = expr[1:]
+        if len(expr) == 3:
+            operator = expr[1][1]
+            if operator == "+":
+                return self.eval_term(expr[0]) + self.eval_expr(expr[2])
+            elif operator == "-":
+                return self.eval_term(expr[0]) - self.eval_expr(expr[2])
+        elif len(expr) == 1:
+            return self.eval_term(expr[0])
+
+    def eval_term(self, term):
+        if len(term) == 2:
+            return self.eval_factor(term)
+        elif len(term) == 3:
+            operator = term[1][1]
+            if operator == "*":
+                return self.eval_factor(term[0]) * self.eval_term(term[2])
+            elif operator == "/":
+                return self.eval_factor(term[0]) / self.eval_term(term[2])
+            eval("1+2*3")
+
+    def eval_factor(self, factor):
+        if factor[0] == "NUM":
+            return self.eval_number(factor[1])
+        elif factor[0] == "VAR":
+            return self.symbols[factor[1]]
+
+    def eval_number(self, number):
+        return int(number)
+
+    def eval_var_expr(self, var_expr):
+        var = self.eval_var(var_expr[0])
+        self.symbols[var] = self.eval_expr(var_expr[2])
+
+    def eval_var(self, var):
+        return var[1]
 
 if __name__ == "__main__":
 
     program = r'''
-        10 LET A = 0
-        20 PRINT A + 1, A+2, A+3
+        10 PRINT "What's your name"
+        20 INPUT A
+        30 PRINT "Hello ", A
     '''
 
     tb = TinyBasic()
     tb.parse(program)
+    #pprint(tb.ast)
     tb.eval()
